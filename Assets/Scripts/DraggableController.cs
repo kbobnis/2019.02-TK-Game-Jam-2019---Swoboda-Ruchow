266e202bcdc6a6f8c77c9fb3c5a64c3c61c9e5ad
Łukasz Biel <lukasz.biel@anixe.pl>
2019-02-23 14:35:04 +0100

    Add line prefab

--- Assets/Scripts/DraggableController.cs
+++ Assets/Scripts/DraggableController.cs
@@ -1,32 +1,29 @@
 using UnityEngine;
 
-namespace DefaultNamespace
+public class DraggableController : MonoBehaviour
 {
-    public class DraggableController : MonoBehaviour
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private Transform transform;
+
+    private Camera mainCamera;
+
+    private void Start()
     {
-        private Vector3 screenPoint;
-        private Vector3 offset;
+        this.transform = GetComponent<Transform>();
+        this.mainCamera = Camera.main;
+    }
 
-        private Transform transform;
+    private void OnMouseDown()
+    {
+        screenPoint = mainCamera.WorldToScreenPoint(transform.position);
+        offset = transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
 
-        private Camera mainCamera;
-
-        private void Start()
-        {
-            this.transform = GetComponent<Transform>();
-            this.mainCamera = Camera.main;
-        }
-
-        private void OnMouseDown()
-        {
-            screenPoint = mainCamera.WorldToScreenPoint(transform.position);
-            offset = transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-        }
-
-        private void OnMouseDrag(){
-            var cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-            var cursorPosition = mainCamera.ScreenToWorldPoint(cursorPoint) + offset;
-            transform.position = cursorPosition;
-        }
+    private void OnMouseDrag(){
+        var cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        var cursorPosition = mainCamera.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
     }
 }
--- Assets/Scripts/LineView.cs
+++ Assets/Scripts/LineView.cs
@@ -13,10 +13,31 @@ public class LineView : MonoBehaviour
     {
         this.lineRenderer = GetComponent<LineRenderer>();
         this.transform = GetComponent<Transform>();
+
+        if (this.points.Length != 0) return;
+
+        this.points = this.transform.GetComponentsInChildren<Transform>();
+
+        Debug.Log(string.Concat(points.Select(p => p.name)));
+
+        var joints = this.transform.GetComponentsInChildren<DistanceJoint2D>();
+        var rigidBodies = this.transform.GetComponentsInChildren<Rigidbody2D>();
+        for (var i = 0; i < rigidBodies.Length - 1; i++)
+        {
+            joints[i + 1].connectedBody = rigidBodies[i];
+        }
     }
 
-    public void Update()
+    private void Update()
     {
-        lineRenderer.SetPositions(new [] { transform.position }.Concat(points.Select(t => t.position)).ToArray());
+        var vertices = GetConnectionPoints();
+
+                                  lineRenderer.positionCount = vertices.Length;
+        lineRenderer.SetPositions(vertices);
+    }
+
+    private Vector3[] GetConnectionPoints()
+    {
+        return this.points.Select(t => t.position).ToArray();
     }
 }
