6bd74d09e0d2f5083c53e75d1c7b1eb567b05014
Łukasz Biel <lukasz.biel@anixe.pl>
2019-02-23 12:19:57 +0100

    Add line renderer

---
+++ Assets/Scripts/LineView.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineView : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[]  points;
+    private LineRenderer lineRenderer;
+    private Transform    transform;
+
+    private void Start()
+    {
+        this.lineRenderer = GetComponent<LineRenderer>();
+        this.transform = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        lineRenderer.SetPositions(new [] { transform.position }.Concat(points.Select(t => t.position)).ToArray());
+    }
+}
