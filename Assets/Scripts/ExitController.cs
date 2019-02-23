aceebbab654e65f2ab784b6e80bf1ec673d57757
Łukasz Biel <lukasz.biel@anixe.pl>
2019-02-23 12:52:34 +0100

    Add exit and exit controller

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public delegate void ExitEvent();
+
+public class ExitController : MonoBehaviour
+{
+    private const string PlayerTag = "Player";
+
+    [SerializeField]
+    private int partsNeededToWin = 8;
+
+    private int partsIn = 0;
+
+    public event ExitEvent OnPlayerExit;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(PlayerTag)) return;
+
+        partsIn++;
+        if (partsIn >= partsNeededToWin)
+        {
+            SendExitEvent();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(PlayerTag)) partsIn--;
+    }
+
+    private void SendExitEvent()
+    {
+        Debug.Log("Player exited");
+        OnPlayerExit?.Invoke();
+    }
+}
