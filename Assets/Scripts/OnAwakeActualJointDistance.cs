fb96fc352be6bfdf5eca769f61996d8c80ee9e44
Krzysztof Bobnis <kbobnis@gmail.com>
2019-02-23 20:35:08 +0100

    joint springs and new graphics

--- Assets/Scripts/OnAwakeActualJointDistance.cs
+++ Assets/Scripts/OnAwakeActualJointDistance.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-    [RequireComponent(typeof(DistanceJoint2D))]
+    //[RequireComponent(typeof(DistanceJoint2D))]
     public class OnAwakeActualJointDistance : MonoBehaviour
     {
         private void Awake()
         {
             DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
-            joint.distance = Vector2.Distance(transform.position, joint.connectedBody.transform.position);
+            if (joint != null)
+            {
+                joint.distance = Vector2.Distance(transform.position, joint.connectedBody.transform.position);
+            }
+            
+            if (joint == null)
+            {
+                SpringJoint2D sjoint = GetComponent<SpringJoint2D>();
+                sjoint.distance = Vector2.Distance(transform.position, sjoint.connectedBody.transform.position);
+            }
+            
+            
 
         }
     }
