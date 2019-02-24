7635426b10a50d644336bb072cc657a094d900bb
Łukasz Biel <lukasz.biel@anixe.pl>
2019-02-24 03:04:41 +0100

    FIx bg

--- Assets/Scripts/GamePadController.cs
+++ Assets/Scripts/GamePadController.cs
@@ -60,7 +60,7 @@ namespace DefaultNamespace
 //                    ChangeController();
 //                }
 
-                playerControllers[currentIndex].Move(LX, LY);
+                current.Move(LX, LY);
             }
         }
 
